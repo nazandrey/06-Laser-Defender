95200efe0c84fb22adfee462eb7785e98b579b80
nazandrey <nazandrbox@yandex.ru>
2020-11-28 19:58:39 +0300

    make background scrolling vertically

---
+++ Laser Defender/Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(Renderer))]
+    public class BackgroundScrolling : MonoBehaviour
+    {
+        [SerializeField] 
+        private float scrollingSpeed;
+
+        private Material _material;
+        
+        private void Awake()
+        {
+            _material = GetComponent<Renderer>().material;
+        }
+
+        private void Update()
+        {
+            _material.mainTextureOffset = new Vector2(0, scrollingSpeed * Time.time);
+        }
+    }
+}
